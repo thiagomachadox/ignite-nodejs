555876f4db30e246aadaedbcd599d7a9cda6df70
Thiago Izidoro <thiagomachadox@gmail.com>
2021-11-09 19:09:04 -0300

    refactor: update server.ts to import data from router from routes/index.ts

--- chapter-02/rentalx/src/server.ts
+++ chapter-02/rentalx/src/server.ts
@@ -1,13 +1,10 @@
 import express from "express";
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(router)
 
 app.listen(3333, "0.0.0.0", () =>
     console.log(`Server is running on port 3333`)
